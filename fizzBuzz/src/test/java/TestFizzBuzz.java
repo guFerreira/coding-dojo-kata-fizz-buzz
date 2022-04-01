c86a9350c359fc48af42c04cadca53c8d8fa8957
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-04-01 00:58:00 -0300

    Implementação do teste que verifica se o método retorna uma string do número de entrada quando não for múltiplo de 3

--- fizzBuzz/src/test/java/TestFizzBuzz.java
+++ fizzBuzz/src/test/java/TestFizzBuzz.java
@@ -1,3 +1,4 @@
+import kata.FizzBuzz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -5,12 +6,21 @@ import org.junit.jupiter.api.Test;
 public class TestFizzBuzz {
 
     @Test
-    @DisplayName("Verifica se o método retorna Fizz se o número de entrada for múltiplo de 3")
-    void verifyNumberIsFizzWhenNumberIsMultipleOfThree(){
+    @DisplayName("Verificar se retorna \"Fizz\" quando o número passado for múltiplo de 3.")
+    void verifyIsFizzWhenNumberIsMultipleOfThree(){
         int numberMultipleOfThree = 3;
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.verifyNumberIsFizz(numberMultipleOfThree);
         Assertions.assertEquals("Fizz", result);
+    }
+    @Test
+    @DisplayName("Verificar se retorna o número de entrada como string quando" +
+            " o número passado não for múltiplo de 3.")
+    void verifyReturnIsNumberWhenEntryIsNotMultipleOfThree(){
+        int numberNonMultipleOfThree = 2;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.verifyNumberIsFizz(numberNonMultipleOfThree);
+        Assertions.assertEquals(Integer.toString(numberNonMultipleOfThree), result);
     }
 
 }
