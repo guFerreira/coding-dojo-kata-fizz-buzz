5913ef5e9b763ad8afb2feb58d76fe70df0d8a24
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-04-01 02:37:19 -0300

    testando a transformação de um número em string em um único método

--- fizzBuzz/src/test/java/TestFizzBuzz.java
+++ fizzBuzz/src/test/java/TestFizzBuzz.java
@@ -1,46 +1,39 @@
 import kata.FizzBuzz;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestFizzBuzz {
 
     @Test
-    @DisplayName("Verificar se retorna \"Fizz\" quando o número passado for múltiplo de 3.")
     void verifyIsFizzWhenNumberIsMultipleOfThree(){
         int numberMultipleOfThree = 3;
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.verifyNumberIsFizz(numberMultipleOfThree);
+        String result = fizzBuzz.convertNumberToFizzBuzzFormat(numberMultipleOfThree);
         Assertions.assertEquals("Fizz", result);
     }
 
     @Test
-    @DisplayName("Verificar se retorna o número de entrada como string quando" +
-            " o número passado não for múltiplo de 3.")
-    void verifyReturnIsNumberWhenEntryIsNotMultipleOfThree(){
-        int numberNonMultipleOfThree = 2;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.verifyNumberIsFizz(numberNonMultipleOfThree);
-        Assertions.assertEquals(Integer.toString(numberNonMultipleOfThree), result);
-    }
-
-    @Test
-    @DisplayName("Verificar se retorna \"Buzz\" quando o número passado for múltiplo de 5.")
     void verifyIsBuzzWhenNumberIsMultipleOfFive(){
         int numberMultipleOfFive = 5;
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.verifyNumberIsBuzz(numberMultipleOfFive);
+        String result = fizzBuzz.convertNumberToFizzBuzzFormat(numberMultipleOfFive);
         Assertions.assertEquals("Buzz", result);
     }
 
     @Test
-    @DisplayName("Verificar se retorna o número de entrada como string quando" +
-            " o número passado não for múltiplo de 5.")
-    void verifyReturnIsNumberWhenEntryIsNotMultipleOfFive(){
-        int numberNonMultipleOfFive = 3;
+    void verifyNumberIsMultipleOfThreeAndFive(){
+        int numberNonMultipleOfFive = 15;
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.verifyNumberIsBuzz(numberNonMultipleOfFive);
-        Assertions.assertEquals(Integer.toString(numberNonMultipleOfFive), result);
+        String result = fizzBuzz.convertNumberToFizzBuzzFormat(numberNonMultipleOfFive);
+        Assertions.assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    void verifySizeListOfResultsAfterPrinter(){
+        int expectedListSize = 100;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.printerResult();
+        Assertions.assertEquals(expectedListSize, fizzBuzz.getResults().size());
     }
 
 }
