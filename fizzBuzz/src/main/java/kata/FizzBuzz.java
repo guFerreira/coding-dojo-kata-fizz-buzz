3aa77ebfc6db430c67d5109f37709dfee19c9fc0
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-04-01 02:39:42 -0300

    implementação do método adiciona os resultados  em uma lista de Strings

--- fizzBuzz/src/main/java/kata/FizzBuzz.java
+++ fizzBuzz/src/main/java/kata/FizzBuzz.java
@@ -1,23 +1,54 @@
 package kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
-    public String verifyNumberIsFizz(int number){
+
+    private List<String> results = new ArrayList<>();
+
+    public void printerResult(){
+        this.generateResults();
+        for (String result: results) {
+            System.out.println(result);
+        }
+    }
+
+    private void generateResults(){
+        for (int i = 1; i <= 100; i ++){
+            this.results.add(this.convertNumberToFizzBuzzFormat(i));
+        }
+    }
+
+    public String convertNumberToFizzBuzzFormat(int number){
+       return "";
+    }
+
+    private String verifyNumberIsFizz(int number){
         if (this.verifyNumberIsMultipleOfThree(number))
             return "Fizz";
-        return Integer.toString(number);
+        return "";
     }
 
     private boolean verifyNumberIsMultipleOfThree(int number){
         return number % 3 == 0;
     }
 
-    public String verifyNumberIsBuzz(int number){
+    private String verifyNumberIsBuzz(int number){
         if (this.verifyNumberIsMultipleOfFive(number))
             return "Buzz";
-        return Integer.toString(number);
+        return "";
     }
 
     private boolean verifyNumberIsMultipleOfFive(int number){
         return number % 5 == 0;
+    }
+
+    public List<String> getResults() {
+        return results;
+    }
+
+    public void setResults(List<String> results) {
+        this.results = results;
     }
 }
